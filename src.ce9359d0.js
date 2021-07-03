9fcceb404f980f123b3d70857fe94bb9bfdb4b60
heorhii-aksiuk <81520242+heorhii-aksiuk@users.noreply.github.com>
2021-07-03 14:41:26 +0000

    Deploying to gh-pages from @ heorhii-aksiuk/team-proj-html-css@0bb081652ac5a6d4cd9338cc528672a282dad24f 🚀

--- src.ce9359d0.js
+++ src.ce9359d0.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-html-css/src.1d125a92.js.map
+//# sourceMappingURL=/team-proj-html-css/src.ce9359d0.js.map
