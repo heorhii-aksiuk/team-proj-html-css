38414a516e83853169ca046c40696b3c7739a9fb
Iurii Papushoi <ones.man.it@gmail.com>
2021-07-09 01:31:28 +0300

    + up btn

---
+++ src/js/upbtn.js
@@ -0,0 +1,16 @@
+const mybutton = document.querySelector ("[upbtn]");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
