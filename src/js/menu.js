1a49ce6b379f79e9144e3d032d3fe28e0ab2eb7b
Iurii Papushoi <ones.man.it@gmail.com>
2021-07-08 22:32:34 +0300

    fix modal disappearing by click each item

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,12 +2,32 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mobileProgramClose = document.querySelector("[program-menu-close]");
+  const mobileAboutClose = document.querySelector("[about-menu-close]");
+  const mobileTrainingClose = document.querySelector("[training-menu-close]");
+  const mobileContactsClose = document.querySelector("[contacts-menu-close]");
 
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
 
   mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileProgramClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileAboutClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileTrainingClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileContactsClose.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
